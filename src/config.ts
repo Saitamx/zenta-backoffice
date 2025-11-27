3414089ac01e884feb90fc5f395793786ffe6650
Saitamx <mat.informatica.ubb@gmail.com>
2025-11-27 12:20:07 -0300

    cors

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,6 @@
-export const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:3000/api";
+const DEFAULT_PROD_API = "https://zenta-backend-production.up.railway.app/api";
+const DEFAULT_DEV_API = "http://localhost:3000/api";
 
-
+export const API_BASE_URL =
+  process.env.REACT_APP_API_URL ||
+  (process.env.NODE_ENV === "production" ? DEFAULT_PROD_API : DEFAULT_DEV_API);
